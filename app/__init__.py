06698131cd5d40b4f7ef1b9841a9a921fd8148f3
Krasivaya <semwagacarine@gmail.com>
2020-04-28 23:34:17 +0200

    Setup Dev Configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from .config import DevConfig
 
 #Initializing application
 app = Flask(__name__)
+
+#Setup configurations
+app.config.from_object(DevConfig)
+
 
 from app import views
