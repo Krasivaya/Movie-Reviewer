0768d0745fc0f615a3e3a4d1de9461c0ba541dcd
Krasivaya <semwagacarine@gmail.com>
2020-04-28 23:25:14 +0200

    Add movie view function

--- app/views.py
+++ app/views.py
@@ -6,3 +6,8 @@ from app import app
 def index():
     message = 'Hello world'
     return render_template('index.html', message = message)
+
+#Dynamic Routes
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+    return render_template('movie.html', id = movie_id)
