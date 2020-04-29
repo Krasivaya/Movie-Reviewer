d05f6313e69ca47d9c325147aad76a3f2026b408
Krasivaya <semwagacarine@gmail.com>
2020-04-29 11:02:14 +0200

    Add movie list object

--- app/request.py
+++ app/request.py
@@ -24,3 +24,21 @@ def get_movie(category):
             movie_results = process_results(movie_results_list)
 
     return movie_results
+
+def process_results(movie_list):
+    movie_results = []
+
+    for movie_item in movie_list:
+        id = movie_item.get('id')
+        title = movie_item.get('original_title')
+        overview = movie_item.get('overview')
+        poster = movie_item.get('poster_path')
+        vote_average = movie_item.get('vote_average')
+        vote_count = movie_item.get('vote_count')
+
+        if poster:
+            movie_object = Movie(id, title, overview, poster, vote_average, vote_count)
+
+            movie_results.append(movie_object)
+        
+    return movie_results
