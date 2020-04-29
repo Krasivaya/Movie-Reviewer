6fe8a249e09990a6e192b5bdcb10204f5c19377b
Krasivaya <semwagacarine@gmail.com>
2020-04-29 16:55:23 +0200

    Add static files

--- app/config.py
+++
@@ -1,8 +0,0 @@
-class Config():
-    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
-
-class ProdConfig(Config):
-    pass
-
-class DevConfig(Config):
-    DEBUG = True
--- app/error.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from app import app
-
-#Errors
-@app.errorhandler(404)
-def four_o_four(error):
-    return render_template(
-        'fourOfour.html'
-    ),404
