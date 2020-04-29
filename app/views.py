122913a18c845568134e3e868efdbf37ff1bbfe2
Krasivaya <semwagacarine@gmail.com>
2020-04-29 11:26:35 +0200

    Add popular movie category

--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 #Views
 @app.route('/')
 def index():
+    popular_movies = get_movies('popular')
+    print(popular_movies)
     title = 'Home - Welcome to the best Movie Review Website Online'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title, popular = popular_movies)
 
 #Dynamic Routes
 @app.route('/movie/<int:movie_id>')
