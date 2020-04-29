59d955cd550108e33f40014856a2fbd882c84360
Krasivaya <semwagacarine@gmail.com>
2020-04-29 14:11:05 +0200

    Add search query

--- app/views.py
+++ app/views.py
@@ -1,20 +1,30 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
 from .request import get_movies, get_movie, search_movie
 
 # HomePage
 @app.route('/')
 def index():
+    # Get Movie Categories
     popular_movies = get_movies('popular')
     upcoming_movies = get_movies('upcoming')
     now_showing_movies = get_movies('now_playing')
+
+    # Title
     title = 'Home - Welcome to the best Movie Review Website Online'
-    return render_template(
-        'index.html', 
-        title = title, 
-        popular = popular_movies,
-        upcoming = upcoming_movies,
-        now_showing = now_showing_movies)
+
+    #Search Movie
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search', movie_name = search_movie))
+    else:
+        return render_template(
+            'index.html', 
+            title = title, 
+            popular = popular_movies,
+            upcoming = upcoming_movies,
+            now_showing = now_showing_movies)
 
 # Movie details
 @app.route('/movie/<int:id>')
