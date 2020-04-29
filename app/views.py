d4701b53f2fa9c21e7b458086c1f4b67cfc1c0d6
Krasivaya <semwagacarine@gmail.com>
2020-04-29 15:03:39 +0200

    Add new review view function

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from app import app
 from .request import get_movies, get_movie, search_movie
+from .models import reviews
+from .forms import ReviewForm
+Review = reviews.Review
 
 # HomePage
 @app.route('/')
@@ -47,4 +50,25 @@ def search(movie_name):
         'search.html',
         title = title,
         movies = searched_movie
+    )
+
+# Movie Form
+@app.route('/movie/review/new/<int:id>', methods = ['GET', 'POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id, title, movie.poster, review)
+        new_review.save_review()
+        return redirect(url_for('movie', id = movie.id))
+
+    title = f'{ movie.title } review'
+    return render_template(
+        'new_review.html',
+        title = title,
+        review_form = form,
+        movie = movie
     )
