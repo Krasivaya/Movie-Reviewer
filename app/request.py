8b61ae6464d74730cdba82108504e0fcf5d64f89
Krasivaya <semwagacarine@gmail.com>
2020-04-29 10:25:47 +0200

    Add movie base url

--- app/request.py
+++ app/request.py
@@ -1,4 +1,11 @@
 from app import app
+import urllib.request,json
+from .models import movie
+
+Movie = movie.Movie
 
 #Get API Key
 api_key = app.config['MOVIE_API_KEY']
+
+#Get Movie Base URL
+base_url = app.config['MOVIE_API_BASE_URL']
