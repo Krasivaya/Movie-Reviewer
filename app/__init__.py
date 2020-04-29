f6f8056bc16ae9c6f0c4e4a7e4de9ad00cb025d0
Krasivaya <semwagacarine@gmail.com>
2020-04-29 16:17:33 +0200

    Add configuration request

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,6 @@ def create_app(config_name):
     #Initializing application
     app = Flask(__name__)
 
-    #Setup configurations
     app.config.from_object(config_options[config_name])
 
     #Initialize flask extensions
@@ -18,5 +17,9 @@ def create_app(config_name):
     #Register Blueprint
     from .main import main 
     app.register_blueprint(main)
+
+    #Setup configurations
+    from .request import configure_request
+    configure_request(app)
 
     return app
