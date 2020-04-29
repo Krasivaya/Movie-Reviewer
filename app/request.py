5103f09926e832f769d49153e10bb46204ac177d
Krasivaya <semwagacarine@gmail.com>
2020-04-29 10:37:26 +0200

    Add api call

--- app/request.py
+++ app/request.py
@@ -9,3 +9,18 @@ api_key = app.config['MOVIE_API_KEY']
 
 #Get Movie Base URL
 base_url = app.config['MOVIE_API_BASE_URL']
+
+def get_movie(category):
+    get_movies_url = base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_movies_url) as url:
+        get_movies_data = url.read()
+        get_movies_response = json.loads(get_movies_data)
+
+        movie_results = None
+
+        if get_movies_response['results']:
+            movie_results_list = get_movies_response['results']
+            movie_results = process_results(movie_results_list)
+
+    return movie_results
