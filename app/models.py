ebc5b9906182be46b9dfbb953329eca7a611dc1b
Krasivaya <semwagacarine@gmail.com>
2020-04-30 13:22:04 +0200

    Add relationship between two models

--- app/models.py
+++ app/models.py
@@ -4,11 +4,13 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(255))
+    users = db.relationship('User', backref = 'role', lazy = 'dynamic')
 
 class Movie:
     def __init__(self, id, title, overview, poster, vote_average, vote_count):
