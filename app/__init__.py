781b65ba23d765260d0b62564786dd020b8df815
Krasivaya <semwagacarine@gmail.com>
2020-04-30 11:06:53 +0200

    Add db instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
 
