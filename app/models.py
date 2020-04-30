746d93536e312f15f3aff605a8b5e2e41bfaaf6e
Krasivaya <semwagacarine@gmail.com>
2020-04-30 14:29:18 +0200

    Add passSecure column

--- app/models.py
+++ app/models.py
@@ -1,11 +1,25 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
-    pass_secure = db.Column(db.String(255))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure, password)
+
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
