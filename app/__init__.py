d87855862c9efaa099f65c8b88af57207112900c
Krasivaya <semwagacarine@gmail.com>
2020-04-30 14:45:24 +0200

    Initialize login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import config_options
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
 
@@ -16,6 +20,7 @@ def create_app(config_name):
     #Initialize flask extensions
     bootstrap.init_app(app)
     db.init_app(app, url_prefix = '/authentication')
+    login_manager.init_app(app)
 
     #Register Blueprint
     from .main import main 
