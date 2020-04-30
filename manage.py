0816116a1da265902e81e33f8d5acd64d08b23fe
Krasivaya <semwagacarine@gmail.com>
2020-04-30 14:03:22 +0200

    Add new manager command

--- manage.py
+++ manage.py
@@ -1,12 +1,14 @@
 from app import create_app, db
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 from app.models import User, Role
 
 # Create app instance
 app = create_app('development')
 
 manager = Manager(app)
-manager.add_command('server', Server)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 # Create app shell
 @manager.shell
