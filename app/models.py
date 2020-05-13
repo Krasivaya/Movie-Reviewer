c25f39008a1810f76e4670915b6d6bd734e9647a
Krasivaya <semwagacarine@gmail.com>
2020-05-13 14:25:48 +0200

    Add review model class

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db, login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -14,6 +15,7 @@ class User(UserMixin,db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
+    reviews = db.relationship('Review', backref = 'user', lazy = 'dynamic')
     pass_secure = db.Column(db.String(255))
 
     @property
@@ -45,28 +47,22 @@ class Movie:
         self.vote_count = vote_count
 
 
-class Review:
-    all_reviews = []
+class Review(db.Model):
+    __tablename__ = 'reviews'
 
-    def __init__(self, movie_id, title, imageurl, review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
+    id = db.Column(db.Integer, primary_key = True)
+    movie_id = db.Column(db.Integer)
+    movie_title = db.Column(db.String)
+    image_path = db.Column(db.String)
+    movie_review = db.Column(db.String)
+    posted = db.Column(db.DateTime, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def save_review(self):
-        Review.all_reviews.append(self)
+        db.session.add(self)
+        db.session.commit()
 
     @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
-
-    @classmethod
-    def get_reviews(cls,id):
-        response = []
-
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
-        
-        return response
+    def get_reviews(cls, id):
+        reviews = Review.query.filter_by(movie_id = id).all()
+        return reviews
