48da432d68a349577de3e1acf0caf94c44be1772
Krasivaya <semwagacarine@gmail.com>
2020-05-13 14:31:45 +0200

    Add movie review model

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app, db
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
-from app.models import User, Role
+from app.models import User, Role, Review
 
 # Create app instance
 app = create_app('development')
