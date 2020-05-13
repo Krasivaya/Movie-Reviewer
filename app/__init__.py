bb2c19a06912b00ab419ef1a185b08ca89c0aefe
Krasivaya <semwagacarine@gmail.com>
2020-05-13 15:34:33 +0200

    Add simple markdown editor instance

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 from config import config_options
 
 # Instances
@@ -14,6 +15,7 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos', IMAGES)
 mail = Mail()
+simple = SimpleMDE()
 
 def create_app(config_name):
 
@@ -27,6 +29,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    simple.init_app(app)
 
     #Register Blueprint
     from .main import main 
