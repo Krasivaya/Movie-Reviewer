0ec8c855b40d879e5e736da44ad82503f5bda25b
Krasivaya <semwagacarine@gmail.com>
2020-05-13 13:51:11 +0200

    Add mail instance

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
 from config import config_options
 
 # Instances
@@ -12,6 +13,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos', IMAGES)
+mail = Mail()
 
 def create_app(config_name):
 
@@ -24,6 +26,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     #Register Blueprint
     from .main import main 
