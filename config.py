f2733b8acd4eb3770639779df66e5051143d909c
Krasivaya <semwagacarine@gmail.com>
2020-05-13 13:26:13 +0200

    Add destination to store images

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ class Config():
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://carine:bananas12@localhost/reviewer'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
 class ProdConfig(Config):
     pass
