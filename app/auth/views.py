3d922e8b0821535ec36d9c7587b415812c912e00
Krasivaya <semwagacarine@gmail.com>
2020-05-13 10:19:47 +0200

    Add login for registered users

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,24 @@
-from flask import render_template,redirect, url_for
+from flask import render_template,redirect, url_for, flash, request
 from . import auth
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from .. import db
 
 @auth.route('/login')
 def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+    title = "Movie Reviewer Login"
     return render_template(
-        '/auth/login.html'
+        '/auth/login.html',
+        login_form = login_form,
+        title = title
     )
 @auth.route('/register', methods = ['GET', 'POST'])
 def register():
@@ -20,5 +31,6 @@ def register():
         title = "New Account"
     return render_template(
         'auth/register.html',
-        registration_form = form
+        registration_form = form,
+        title = title
     )
