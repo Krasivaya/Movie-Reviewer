0a48b2f69bdd181e2b5446101c9f121b62b93e7a
Krasivaya <semwagacarine@gmail.com>
2020-05-13 13:48:14 +0200

    Add email configurations

--- config.py
+++ config.py
@@ -8,6 +8,13 @@ class Config():
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://carine:bananas12@localhost/reviewer'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
+    # Email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
 class ProdConfig(Config):
     pass
 
